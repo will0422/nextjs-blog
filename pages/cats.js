a0cfa612678f03cbbdbf4e1c73a100916aa7cfcd
will0422 <williamchin2005@gmail.com>
2023-03-05 20:15:52 -0800

    final

--- pages/cats.js
+++ pages/cats.js
@@ -1,36 +1,30 @@
 import utilStyles from 'C:/Users/bobch/Documents/NextJS App/nextjs-blog/styles/utils.module.css';
 import Layout from 'C:/Users/bobch/Documents/NextJS App/nextjs-blog/components/layout.js';
+const fetch = import('node-fetch');
 
 export default function Cats() {
+    catPics()
     return (
         <Layout>
             <center>
                 <h1 className={utilStyles.headingLg}>Click for a picture of a cat!</h1>
 
-                <script>fetchPics();</script>
+                <button class="btn btn-primary my-2 generate_btn">Generate</button>
+
+                <script>fetchPics()</script>
 
                 <div class="catsImgDiv"></div>
 
             </center>
         </Layout>
     );
-}
 
-function fetchPics() {
-    let catsImgDiv = document.querySelector(".catsImgDiv")
-    catsImgDiv.innerHTML = ''
 
-    fetch('https://api.thecatapi.com/v1/images/search')
-        .then(response => response.json())
-        .then((data) => {
-            let catsImgUrl = data[0].catsImgUrl
 
-            letCatsImgEl = document.createElement("img")
-            catsImgEl.setAttribute('src', '${catsImgUrl]')
-            catsImgEl.classList.add("showcase")
-
-            let catsImgDiv = document.querySelector(".catsImgDiv")
-            catsImgDiv.appendChild(catsImgEl)
-        })
-        .catch(err => console.log(err))
+    function catPics() {
+        fetch('https://api.thecatapi.com/v1/images/search%27')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
 }
